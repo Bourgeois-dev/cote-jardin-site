01bb5734a6c3965fc8de5ba102e6ecad1d5cfe9a
Bourgeois-dev <bourgeois.v92@gmail.com>
2026-06-24 23:59:59 +0200

    Maj

--- src/components/site/Navbar.tsx
+++ src/components/site/Navbar.tsx
@@ -10,8 +10,8 @@ export default function Navbar({ onReserve, reserveLabel = "Réserver", flags }:
         <a className="brand" href="#">{import.meta.env.VITE_RESTO_NAME || "Restaurant"}</a>
         <ul className="nav-links">
           <li><a href="#histoire">Notre cuisine</a></li>
-          <li><a href="#carte">La carte</a></li>
           {f.ardoise && <li><a href="#jour">Plat du jour</a></li>}
+          <li><a href="#carte">La carte</a></li>
           {f.takeaway && <li><a href="#emporter">À emporter</a></li>}
           {f.partners && <li><a href="#producteurs">Producteurs</a></li>}
           <li><a href="#galerie">Galerie</a></li>
